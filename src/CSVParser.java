b9111ce446167573cf85a55e650646021e26b090
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2022-01-25 14:11:49 -0600

    Some new parsing logic

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -1,9 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class CSVParser {
+	
+	private static List<String> getRecordFromLine(String line) {
+	    List<String> values = new ArrayList<String>();
+	    try (Scanner rowScanner = new Scanner(line)) {
+	        rowScanner.useDelimiter(COMMA_DELIMITER);
+	        while (rowScanner.hasNext()) {
+	            values.add(rowScanner.next());
+	        }
+	    }
+	    return values;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		List<List<String>> records = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File("book.csv"));) {
+		    while (scanner.hasNextLine()) {
+		        records.add(getRecordFromLine(scanner.nextLine()));
+		    }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}	
 
 }
